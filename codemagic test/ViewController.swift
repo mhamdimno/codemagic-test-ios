8c1408a1f9e8dd0fc1729b4df2c9202b4ae5c702
bob <bob@>
2023-08-26 06:26:07 +0300

    Initial Commit

---
+++ codemagic test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  codemagic test
+//
+//  Created by A One Way To Allah on 26/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
